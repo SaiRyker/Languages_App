e6b61a866696f0b08c250c5db256b1998fddcee0
DanilChernyshov <daniilchernyshov0@gmail.com>
2025-05-14 17:46:10 +0300

    add PrTask and Language module import, export prSolutionService

--- BackLang/nest-backend-langs/src/pr_solutions/pr_solutions.module.ts
+++ BackLang/nest-backend-langs/src/pr_solutions/pr_solutions.module.ts
@@ -7,12 +7,19 @@ import {PrSolution} from "./pr_solutions.model";
 import {User} from "../users/user.model";
 import {Lesson} from "../lessons/lessons.model";
 import {Language} from "../prog_langs/prog_langs.model";
+import {PrTasksService} from "../pr_tasks/pr_tasks.service";
+import {PrTasksModule} from "../pr_tasks/pr_tasks.module";
+import {ProgLangsModule} from "../prog_langs/prog_langs.module";
 
 @Module({
   providers: [PrSolutionsService],
   controllers: [PrSolutionsController],
   imports: [
     SequelizeModule.forFeature([PrTask, PrSolution, User, Lesson, Language]),
+    PrTasksModule, ProgLangsModule
   ],
+  exports: [
+    PrSolutionsService
+  ]
 })
 export class PrSolutionsModule {}
