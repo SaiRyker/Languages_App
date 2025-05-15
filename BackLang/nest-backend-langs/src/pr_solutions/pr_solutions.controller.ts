ee8d8a4c992c8142da8747e40389484d600de74e
DanilChernyshov <daniilchernyshov0@gmail.com>
2025-05-15 15:35:37 +0300

    add controllers for functions of saving practical solution and its result

--- BackLang/nest-backend-langs/src/pr_solutions/pr_solutions.controller.ts
+++ BackLang/nest-backend-langs/src/pr_solutions/pr_solutions.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import {User} from "dockerode";
+import {PrSolution} from "./pr_solutions.model";
+import {PrSolutionsService} from "./pr_solutions.service";
 
-@Controller('pr-solutions')
-export class PrSolutionsController {}
+@Controller('prsolutions')
+export class PrSolutionsController {
+    constructor(private prSolutionsService: PrSolutionsService) {}
+
+    @Get(':prTask_id/:student_id')
+    async getUserPrSolution(@Param('prTask_id') prTask_id: number, @Param('student_id') student_id: number) {
+        return this.prSolutionsService.getUserPrSolution(prTask_id, student_id);
+    }
+
+    @Post('/run-submit')
+    async runSubmit(@Body() body: { code: string; language: string; testCode: string;}){
+        const { code, language, testCode} = body;
+        return this.prSolutionsService.runCodeWithTests(code, language, testCode);
+    }
+
+    @Post('/user-submit')
+    async saveUserPrSolution(@Body() body: {prTaskId: number, studentId: number, code: string, lang_name: string}){
+        const { prTaskId, studentId, code, lang_name} = body;
+        return this.prSolutionsService.saveUserPrSolution(prTaskId, studentId, code, lang_name);
+    }
+}
