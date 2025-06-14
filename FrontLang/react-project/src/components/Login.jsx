ddf8070025abb0f6009e0fd43d5067d5efcc0272
DanilChernyshov <daniilchernyshov0@gmail.com>
2025-06-14 16:20:40 +0300

    add styles for Login page

--- FrontLang/react-project/src/components/Login.jsx
+++ FrontLang/react-project/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { login } from '../api/authApi';
+import '../App.css';
 
 function Login() {
     const [email, setEmail] = useState('');
@@ -21,20 +22,25 @@ function Login() {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                </div>
-                <button type="submit">Войти</button>
-            </form>
+        <div className="login-container">
+            <div className="login-wrapper">
+            <img src="src/assets/logo.png" alt="Site Logo" className="site-logo"/>
+            <div className="login-form">
+                <h2>Вход в аккаунт</h2>
+                {error && <p className="error-message">{error}</p>}
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        {/*<label>Email или логин:</label>*/}
+                        <input placeholder="Введите логин или Email" className="form-input" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                    </div>
+                    <div className="form-group">
+                        {/*<label>Пароль:</label>*/}
+                        <input placeholder="Введите пароль" className="form-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                    </div>
+                    <button className="submit-button" type="submit">Войти</button>
+                </form>
+            </div>
+            </div>
         </div>
     );
 }
