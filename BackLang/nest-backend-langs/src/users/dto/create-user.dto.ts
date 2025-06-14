d477bfa176c8c0ddee4a0bb7b4e0d3c80093c5c5
DanilChernyshov <daniilchernyshov0@gmail.com>
2025-06-14 16:02:19 +0300

    add role_name and user_fio to user dto

--- BackLang/nest-backend-langs/src/users/dto/create-user.dto.ts
+++ BackLang/nest-backend-langs/src/users/dto/create-user.dto.ts
@@ -7,6 +7,10 @@ export class CreateUserDto {
     //@IsEmail({}, {message: 'Некорректный email'})
     readonly user_email: string;
     //@IsString({message: 'Должно быть строкой'})
+    readonly role_name: string;
+    //@IsString({message: 'Должно быть строкой'})
+    readonly user_fio?: string;
+    //@IsString({message: 'Должно быть строкой'})
     //@Length(4, 16, {message: 'Пароль должен быть не меньше 4 и не больше 16 символов'})
-    readonly user_password: string;
+    readonly user_password?: string;
 }
